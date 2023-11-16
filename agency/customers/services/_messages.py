23471171b104e627421c4ba4c1b0c79db36b76ef
Bogdan Loginov <bogdanloginov31@gmail.com>
2023-11-16 22:15:24 +0300

    Fix email messages paths

--- agency/customers/services/_messages.py
+++ agency/customers/services/_messages.py
@@ -1,9 +1,16 @@
+from django.conf import settings
+
+
 ERROR_MAIL_SEND = "Some error with mail sending error occured"
 
 
-with (open("templates\\messages\\order-thanks.html", "rb") as html_layout):
+with open(
+        settings.BASE_DIR / "templates\\messages\\order-thanks.html", "rb"
+) as html_layout:
     ORDER_THANKS = str(html_layout.read())
 
 
-with (open("templates\\messages\\order-admin.html", "rb") as html_layout):
+with open(
+        settings.BASE_DIR / "templates\\messages\\order-admin.html", "rb"
+) as html_layout:
     ADMIN_ORDER_NOTIFICATION = str(html_layout.read())
