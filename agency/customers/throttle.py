e6c435e14af4bd1e4381f3589b237c127833263f
Bogdan Loginov <bogdanloginov31@gmail.com>
2023-11-18 13:49:01 +0300

    Fix UI

--- agency/customers/throttle.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.throttling import AnonRateThrottle
-
-
-CUSTOMER_CONTACTS_SEND_THROTTLE_RATE = "10/day"
-
-
-class CustomerContactsSendThrottle(AnonRateThrottle):
-    def get_rate(self):
-        return CUSTOMER_CONTACTS_SEND_THROTTLE_RATE
