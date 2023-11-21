62fb09f0c55af5af215043a56ce7468b50431291
Bogdan Loginov <bogdanloginov31@gmail.com>
2023-11-21 15:42:27 +0300

    Fix ip util

--- agency/customers/services/ip.py
+++ agency/customers/services/ip.py
@@ -3,4 +3,4 @@ from django.http.request import HttpRequest
 
 def get_ip_from_request(request: HttpRequest) -> str:
     return request.META.get("HTTP_X_FORWARDED_FOR",
-                            self.request.META.get("REMOTE_ADDR"))
+                            request.META.get("REMOTE_ADDR"))
