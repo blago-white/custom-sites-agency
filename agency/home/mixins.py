9a35ffd7db5552128e56d0d8843d113b85fd177f
Bogdan Loginov <bogdanloginov31@gmail.com>
2023-12-12 23:59:43 +0300

    Fix url-internationalization

--- agency/home/mixins.py
+++ agency/home/mixins.py
@@ -1,16 +1,25 @@
-from django.utils.translation import get_language
+from django.utils import translation
 from django.conf import settings
 
 from . import config
 
 
 class MultilangHomeViewMixin:
+    _selected_language: str = None
+
     def get_context_data(self, **kwargs) -> dict:
         context = super().get_context_data(**kwargs)
 
-        context.update({config.LANG_CODE_TEMPLATE_NAME: settings.DEFAULT_LANGUAGE})
+        self._selected_language = self._get_selected_language_or_default()
+        self._update_current_language()
 
-        if config.LANG_CODE_URL_ARG_NAME in self.kwargs:
-            context[config.LANG_CODE_TEMPLATE_NAME] = self.kwargs.get(config.LANG_CODE_URL_ARG_NAME)
+        context[config.LANG_CODE_TEMPLATE_NAME] = self._selected_language
 
         return context
+
+    def _update_current_language(self) -> None:
+        if self._selected_language and self._selected_language != translation.get_language():
+            translation.activate(self._selected_language)
+
+    def _get_selected_language_or_default(self) -> str:
+        return self.kwargs.get(config.LANG_CODE_URL_ARG_NAME) or settings.DEFAULT_LANGUAGE
