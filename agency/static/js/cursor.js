e611c8b16d9a81b377935bacbafad7ad778a4600
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-01-22 16:31:54 +0300

    Small changes ui

--- agency/static/js/cursor.js
+++ agency/static/js/cursor.js
@@ -1,10 +1,6 @@
 const circle = document.getElementById('circle');
 
-//document.addEventListener('mousemove', function(ev){
-//    circle.style.transform = 'translateY('+(ev.clientY-400)+'px)';
-//    circle.style.transform += 'translateX('+(ev.clientX-700)+'px)';
-//},false);
 document.addEventListener('mousemove', function(ev){
-    circle.style.transform = 'translateY('+(ev.clientY-350)+'px)';
-    circle.style.transform += 'translateX('+(ev.clientX-350)+'px)';
+    circle.style.transform = 'translateY('+(ev.clientY-50)+'px)';
+    circle.style.transform += 'translateX('+(ev.clientX-50)+'px)';
 },false);
