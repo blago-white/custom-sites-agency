72cd9b3113840f07b24d1c4cad924ca053892808
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-01-25 02:31:51 +0300

    Update bg

--- agency/static/js/cursor.js
+++ agency/static/js/cursor.js
@@ -1,6 +1,7 @@
 const circle = document.getElementById('circle');
 
 document.addEventListener('mousemove', function(ev){
-    circle.style.transform = 'translateY('+(ev.clientY-50)+'px)';
-    circle.style.transform += 'translateX('+(ev.clientX-50)+'px)';
+    console.log( `translateY(${ev.clientY}px) translateY(-20vh)`)
+    circle.style.transform = `translateY(${ev.clientY}px) translateY(-10vh)`;
+    circle.style.transform += `translateX(${ev.clientX}px) translateX(-10vh)`;
 },false);
