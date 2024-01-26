a455f7d458ac62c9e34766dfc1f8c7e9062522bb
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-01-26 16:50:31 +0300

    Update meta tags

--- agency/static/js/cursor.js
+++ agency/static/js/cursor.js
@@ -1,7 +1,26 @@
+let freezed = false;
+
 const circle = document.getElementById('circle');
 
+const orderButton =  document.getElementsByClassName('contact-button-top')[0];
+
 document.addEventListener('mousemove', function(ev){
-    console.log( `translateY(${ev.clientY}px) translateY(-20vh)`)
+    if (freezed) {
+        return
+    }
+
     circle.style.transform = `translateY(${ev.clientY}px) translateY(-10vh)`;
     circle.style.transform += `translateX(${ev.clientX}px) translateX(-10vh)`;
 },false);
+
+orderButton.addEventListener('mouseover', function(e) {
+    circle.style.transform += "scale(0)";
+
+    freezed = true;
+})
+
+orderButton.addEventListener('mouseout', function(e) {
+    circle.style.transform += "scale(1)";
+
+    freezed = false;
+})
