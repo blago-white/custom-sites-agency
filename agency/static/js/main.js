b232170cc0b1ae716a8c004bb616f27226f44041
Bogdan Loginov <bogdanloginov31@gmail.com>
2024-02-02 15:18:37 +0300

    Add steps section

--- agency/static/js/main.js
+++ agency/static/js/main.js
@@ -1,5 +1,6 @@
 import { expanding } from "./mobileMenu.js";
 
+const stepsContentWrapper = document.getElementById('steps');
 const secondContentWrapper = document.getElementsByClassName('second-content-wrapper')[0];
 const thirdContentWrapper = document.getElementsByClassName('third-content-wrapper')[0];
 
@@ -8,6 +9,10 @@ let colorValue = 36;
 const aspectRatio = window.outerWidth / window.outerHeight;
 
 const secondSectionBlurDivider = aspectRatio > 12/9 ? 25 : 50;
+
+function print(value) {
+    console.log(value)
+}
 
 function onScroll(event) {
     if (expanding()) {
@@ -18,34 +23,83 @@ function onScroll(event) {
     const mainSectionHeight = document.getElementsByClassName('main-content-wrapper')[0].offsetHeight;
     const secondSectionHeight = document.getElementById('prices').parentElement.offsetHeight;
     const thirdSectionHeight = document.getElementById('about').offsetHeight;
+    const stepsSectionHeight = document.getElementById('steps').offsetHeight;
     const processedHeight = 0;
 
-    if (scrollTopHeight > processedHeight) {
-        if (scrollTopHeight > 0 && scrollTopHeight < mainSectionHeight) {
-            document.getElementsByClassName('main-content-wrapper')[0].style.filter = "blur(" + (
-                    (scrollTopHeight - processedHeight) / 25
-                ) + "px)";
+    if (scrollTopHeight > 0 && scrollTopHeight < mainSectionHeight) {
+        document.getElementsByClassName('main-content-wrapper')[0].style.filter = "blur(" + (
+            (scrollTopHeight - processedHeight) / 25
+        ) + "px)";
 
-            secondContentWrapper.style.boxShadow = "0px 0px " + (scrollTopHeight - processedHeight) + "px #dbdbdb";
-        } else {
-            document.getElementsByClassName('main-content-wrapper')[0].style.filter = "blur(0px)";
-            secondContentWrapper.style.boxShadow = "0px 0px 0px #dbdbdb";
-        }
+        stepsContentWrapper.style.boxShadow = "0px 0px " + (scrollTopHeight - processedHeight) + "px #202020";
+    } else if ((scrollTopHeight > stepsSectionHeight) && (scrollTopHeight < (stepsSectionHeight + secondSectionHeight))) {
+        stepsContentWrapper.style.filter = "blur(" + (
+            (scrollTopHeight - stepsSectionHeight) / 100
+        ) + "px)";
 
-        if (scrollTopHeight > secondSectionHeight) {
-            const thirdSectionScrollDelta = scrollTopHeight - secondSectionHeight;
+        secondContentWrapper.style.boxShadow = "0px 0px " + (scrollTopHeight - stepsSectionHeight) + "px #dbdbdb";
+    } else if (scrollTopHeight > (stepsSectionHeight + secondSectionHeight)) {
+        const thirdSectionScrollDelta = scrollTopHeight - (stepsSectionHeight + secondSectionHeight);
 
-            secondContentWrapper.parentElement.style.filter = "blur(" + ((
-                (thirdSectionScrollDelta / secondSectionBlurDivider)
-            ) - 1) + "px)";
+        secondContentWrapper.parentElement.style.filter = "blur(" + ((
+            (thirdSectionScrollDelta / secondSectionBlurDivider)
+        ) - 1) + "px)";
 
-            thirdContentWrapper.style.boxShadow = "0px 0px " + (
-                thirdSectionScrollDelta
-            ) + "px #202020" + ", 0px 0px " + (thirdSectionScrollDelta) + "px #202020";
-        } else {
-            secondContentWrapper.parentElement.style.filter = "";
-        }
+        thirdContentWrapper.style.boxShadow = "0px 0px " + (
+            thirdSectionScrollDelta
+        ) + "px #202020" + ", 0px 0px " + (thirdSectionScrollDelta) + "px #202020";
     }
+
+    if (scrollTopHeight > mainSectionHeight) {
+        print("1");
+        document.getElementsByClassName('main-content-wrapper')[0].style.filter = "blur(0px)";
+        stepsContentWrapper.style.boxShadow = "0px 0px 0px #202020";
+    }
+
+    if (scrollTopHeight > (mainSectionHeight + stepsSectionHeight) || scrollTopHeight <= mainSectionHeight) {
+        print("2");
+        stepsContentWrapper.style.filter = "blur(0px)";
+        secondContentWrapper.style.boxShadow = "0px 0px 0px #dbdbdb";
+    }
+
+    if (scrollTopHeight > (mainSectionHeight + stepsSectionHeight + secondSectionHeight) || scrollTopHeight <= (mainSectionHeight + stepsSectionHeight)) {
+        print("3");
+
+        secondContentWrapper.parentElement.style.filter = "blur(0px)";
+        thirdContentWrapper.style.boxShadow = "0px 0px 0px #202020";
+    }
+
+//
+//    if (scrollTopHeight > processedHeight) {
+//        print(scrollTopHeight);
+//        print(processedHeight);
+//        print('-----')
+//
+//        if (scrollTopHeight > 0 && scrollTopHeight < mainSectionHeight) {
+//            document.getElementsByClassName('main-content-wrapper')[0].style.filter = "blur(" + (
+//                    (scrollTopHeight - processedHeight) / 25
+//                ) + "px)";
+//
+//            secondContentWrapper.style.boxShadow = "0px 0px " + (scrollTopHeight - processedHeight) + "px #dbdbdb";
+//        } else {
+//            document.getElementsByClassName('main-content-wrapper')[0].style.filter = "blur(0px)";
+//            secondContentWrapper.style.boxShadow = "0px 0px 0px #dbdbdb";
+//        }
+//
+//        if (scrollTopHeight > secondSectionHeight) {
+//            const thirdSectionScrollDelta = scrollTopHeight - secondSectionHeight;
+//
+//            secondContentWrapper.parentElement.style.filter = "blur(" + ((
+//                (thirdSectionScrollDelta / secondSectionBlurDivider)
+//            ) - 1) + "px)";
+//
+//            thirdContentWrapper.style.boxShadow = "0px 0px " + (
+//                thirdSectionScrollDelta
+//            ) + "px #202020" + ", 0px 0px " + (thirdSectionScrollDelta) + "px #202020";
+//        } else {
+//            secondContentWrapper.parentElement.style.filter = "";
+//        }
+//    }
 }
 
 document.addEventListener('scroll', onScroll)
